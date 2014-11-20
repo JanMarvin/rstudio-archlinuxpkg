da3467dd853882220722c300d4163b23a03d6571
Marvin <jmg@edna>
2014-11-20 18:36:14 +0100

    boost fixes

--- rstudio/sed.sh
+++ rstudio/sed.sh
@@ -10,7 +10,21 @@ find . \( -name *.cpp -or -name *.cpp.in -or -name *.hpp \) -exec sed \
 
 git add *
 git commit -m "boost fix"
+git diff HEAD^ HEAD > ../boost156.patch
+
+find . \( -name *.cpp -or -name *.cpp.in -or -name *.hpp \) -exec sed \
+        -e 's/boost::signals::/boost::signals2::/g' \
+        -e 's/signals.hpp/signals2.hpp/g' \
+        -e 's/boost::signal</boost::signals2::signal</g' \
+        -e 's/signal.hpp/signals2\/signal.hpp/g' \
+        -e 's/boost::last_value/boost::signals2::last_value/g' \
+        -i {} \;
+
+git add *
+git commit -m "boost signals to boost signals2"
+
+git diff HEAD^ HEAD > ../boostsignals.patch
+
 git checkout master
-git diff master sed > ../boost156.patch
 git branch -D sed
 cd ..
