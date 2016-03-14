49f7105bc6880caeeb0b59f8ba2fd063c7b2d3d8
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2016-03-14 16:55:22 +0100

    Update boostsignals and sed script

--- rstudio/sed.sh
+++ rstudio/sed.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
 cd rstudio
 git checkout -b sed
 
-find . \( -name *.cpp -or -name *.cpp.in -or -name *.hpp \) -exec sed \
+find . \( -name "*.cpp" -o -name "*.cpp.in" -o -name "*.hpp" \) -exec  sed \
         -e 's/boost::signals::/boost::signals2::/g' \
         -e 's/signals.hpp/signals2.hpp/g' \
         -e 's/boost::signal</boost::signals2::signal</g' \
         -e 's/signal.hpp/signals2\/signal.hpp/g' \
         -e 's/boost::last_value/boost::signals2::last_value/g' \
+        -e 's/signals::connection/signals2::connection/g' \
         -i {} \;
 
 git add *
